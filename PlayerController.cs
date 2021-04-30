6805081762a4ed74e2650e67794bda141d39b7dd
swwsiwwe <45661591+swwsiwwe@users.noreply.github.com>
2021-04-30 23:30:52 +0800

    fixed #26

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,31 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class FinalMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Collider2D coll;
     private Animator anim;
+    
 
-    public float speed, jumpForce;
+    private float hp;
+    private float mp;
+
+    [Header("生命")]
+    [SerializeField]
+    private float maxHp;
+
+    [Header("能量")]
+    [SerializeField]
+    private float maxMp;
+    
     private float horizontalMove;
-    public Transform groundCheck;
-    public LayerMask ground;
-    public Transform wallCheck;
 
-    public bool isGround, isJump, isDashing, isClimbing;
+    [Header("速度")]
+    [SerializeField]
+    private float speed;
+    [Header("跳跃力")]
+    [SerializeField]
+    private float jumpForce;
+    [Header("地面检测")]
+    [SerializeField]
+    private Transform groundCheck;
+
+    [Header("图层检测")]
+    [SerializeField]
+    private LayerMask ground;
+
+    [Header("墙面检测")]
+    [SerializeField]
+    private Transform wallCheck;
+
+    [Header("蹬墙跳能力")]
+    [SerializeField]
+    private bool climbAble;
+
+    [Header("冲刺能力")]
+    [SerializeField]
+    private bool rushAble;
+
+    [Header("二段跳能力")]
+    [SerializeField]
+    public bool doubleJumpAble;
+
+    //isJump区别攀爬和第二段跳跃
+    private bool isGround, isJump, isDashing, isClimbing;
 
     bool jumpPressed;
-    [SerializeField] private int jumpCount;
 
+    [Header("最大跳跃次数")]
+    [SerializeField]
+    private int jumpCount;
+
+    [Header("Better Jump重力系数")]
+    [SerializeField]
+    private float jumpPa;
 
     [SerializeField]
     [Header("盒子size")]
     private Vector2 boxSize;
 
+
+    [Header("Hurt")]
+    public float hurtLength; //MARK 自定义计数器长度
+    private float hurtCount;//MARK 计数器 每帧减少
+    [HideInInspector] public bool isAttacked;
+
     // Start is called before the first frame update
     void Start()
     {
+       
+        hp = maxHp;
+        mp = maxMp;
         currentSpeedX = 0;
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<Collider2D>();
@@ -36,6 +90,7 @@ public class FinalMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
 
         if (Input.GetKeyDown(KeyCode.C) && jumpCount > 0)
         {
@@ -43,26 +98,29 @@ public class FinalMovement : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.X))
         {
-            
+            if (Time.time >= (dashInterval + lastDash))
+                ReadyToDash();
         }
 
         cdImage.fillAmount -= 1.0f / dashInterval * Time.deltaTime;
     }
 
     private void FixedUpdate()
-    {     
-        isGround = Physics2D.OverlapBox(groundCheck.position, boxSize, 0, ground);   
+    {
+        tsped = rb.velocity.y;
+        isGround = Physics2D.OverlapBox(groundCheck.position, boxSize, 0, ground);
 
         if (isGround)
         {
             rb.drag = 0;
         }
-        Dash();
-        if (isDashing)
-            return;
-        GroundMovement();
 
+        GroundMovement();
+        BetterJump();
+       
     }
+
+
 
     void GroundMovement()
     {
@@ -73,6 +131,7 @@ public class FinalMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(horizontalMove, 1, 1);
         }
+
         if (currentSpeedX > 0)
         {
             currentSpeedX -= 0.5f;
@@ -85,5 +144,49 @@ public class FinalMovement : MonoBehaviour
         {
 
         }
+
     }
+
+
+    void BetterJump()//跳跃
+    {
+
+        if (isGround)
+        {
+            jumpCount = 2;//可跳跃数量
+            isJump = false;
+        }
+
+        if (jumpPressed && isGround)
+        {
+            isJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        else if (jumpPressed && jumpCount > 0 && isJump)
+        {
+            if (doubleJumpAble)
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+
+        if (rb.velocity.y >= -50 && rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * 2.5f * Time.deltaTime;
+
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.C))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * jumpPa * Time.deltaTime;
+        }
+
+    }
+
+
+
+   
 }
+
+
